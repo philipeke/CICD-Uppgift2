68061a0b7061188bb505f4da62a9213765a15981
Nils Part <Nils.Part@protonmail.com>
2021-11-02 11:37:55 +0100

    Created new function to print first letter in hint

--- src/components/hint/Hint.js
+++ src/components/hint/Hint.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState, useContext} from 'react'
+import { PokemonContext } from '../../shared/provider/PokemonProvider'
 
 export const Hint = () => {
     const [hintText, setHintText] = useState()
+    const [pokemon] = useContext(PokemonContext)
     const showHint = () => {setHintText("null")} 
+    const firstLetterOfPokenomName = () => {
+        const pokemonName = pokemon?.name
+        if(pokemonName !== null)
+        {
+            return pokemonName(0)
+        }
+        return null
+    }
     return (
         <div>
-            <p>{hintText}</p>
+            <p>{firstLetterOfPokenomName()}</p>
             <button className= "input" onClick={() => showHint()}> Hint</button>
         </div>
     )
