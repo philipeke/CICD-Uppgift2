fda14bcf2733b37e62e3323a139c14a886c9d0cf
Karl Sjöstrand <karljsjo@gmail.com>
2021-11-02 12:16:25 +0100

    added score board only visible when revealed > 0

--- src/views/whosthatpokemonview/WhosThatPokemonView.js
+++ src/views/whosthatpokemonview/WhosThatPokemonView.js
@@ -8,14 +8,15 @@ import { ScoreBoard } from '../../components/scoreboard/ScoreBoard'
 import WhosThatPokemonImg from '../../shared/resources/images/whos-that-pokemon.bmp'
 import PokeballImg from '../../shared/resources/images/pokeball.png'
 import NumberUtils from '../../utils/NumberUtils'
-
-import {ThemeSwitch} from '../../components/themeSwitch/ThemeSwitch'
+import { ThemeSwitch } from '../../components/themeSwitch/ThemeSwitch'
+import { ScoreContext } from '../../shared/provider/ScoreProvider'
 
 export const WhosThatPokemonView = () => {
   const history = useHistory()
   const [pokemon, setPokemon] = useContext(PokemonContext)
   const [answer, setAnswer] = useState('')
   const [isLoading, setIsLoading] = useState(true)
+  const { revealed:[revealed] } = useContext(ScoreContext)
 
   useEffect(() => {
 
@@ -75,7 +76,7 @@ export const WhosThatPokemonView = () => {
             value='Reveal'/>
         </form>
       </div>
-      <ScoreBoard />
+      {revealed > 0 ? <ScoreBoard /> : null}
     </div>
   )
 }
